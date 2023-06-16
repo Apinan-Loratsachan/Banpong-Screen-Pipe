5a06c3c8c54d404f657d20403edf0be686d3c899
Apinan Loratsachan <sunseasame2556@gmail.com>
2023-06-16 14:54:04 +0700

    Prototype

---
+++ js/variables.js
@@ -0,0 +1,48 @@
+sect1_title = "Home"
+sect1_link = "index.html"
+sect2_title = "Products"
+sect2_link = "index.html#section_2"
+sect3_title = "Manufacturing"
+sect3_link = "index.html#section_3"
+sect4_title = "Certificate"
+sect4_link = "index.html#section_4"
+sect5_title = "Contact"
+sect5_link = "index.html#section_5"
+
+copyright = "Copyright © 2023 <br>Banpong Screen Pipe Co., Ltd. <br>All rights reserved.";
+facebook_title = "facebook"
+facebook_link = "https://www.facebook.com/bspwell"
+instagram_title = "instagram"
+instagram_link = "https://www.instagram.com/bspwellscreenbanpongscreen/"
+tel_title = "+6632 742203-4"
+tel_link = "tel:+6632742203-4"
+fax_title = "+6632 742202"
+fax_link = "fax:+6632742202"
+
+//--------------------------------------------------------------------------------
+document.getElementById("top_sect1").innerHTML = sect1_title;
+document.getElementById("top_sect1").href = sect1_link;
+document.getElementById("top_sect2").innerHTML = sect2_title;
+document.getElementById("top_sect2").href = sect2_link;
+document.getElementById("top_sect3").innerHTML = sect3_title;
+document.getElementById("top_sect3").href = sect3_link;
+document.getElementById("top_sect4").innerHTML = sect4_title;
+document.getElementById("top_sect4").href = sect4_link;
+document.getElementById("top_sect5").innerHTML = sect5_title;
+
+document.getElementById("footer_sect1").innerHTML = sect1_title;
+document.getElementById("footer_sect1").href = sect1_link;
+document.getElementById("footer_sect2").innerHTML = sect2_title;
+document.getElementById("footer_sect2").href = sect2_link;
+document.getElementById("footer_sect4").innerHTML = sect4_title;
+document.getElementById("footer_sect4").href = sect4_link;
+document.getElementById("footer_sect5").innerHTML = sect5_title;
+document.getElementById("footer_sect5").href = sect5_link;
+
+document.getElementById("copyright").innerHTML = copyright;
+document.getElementById("facebook").innerHTML = facebook_title;
+document.getElementById("facebook").href = facebook_link;
+document.getElementById("instagram").innerHTML = instagram_title;
+document.getElementById("instagram").href = instagram_link;
+document.getElementById("tel").innerHTML = tel_title;
+document.getElementById("tel").href = tel_link;
